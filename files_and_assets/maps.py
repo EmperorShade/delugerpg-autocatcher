6c264c39eae9ec958191f55a2a89252e9a21c240
Emperor Shade <shrirammaiya44@gmail.com>
2023-01-12 11:24:25 +0530

    Added Tower of Lost Souls

--- files_and_assets/maps.py
+++ files_and_assets/maps.py
@@ -3,5 +3,6 @@ maps = ["http://www.delugerpg.com/map/1", "http://www.delugerpg.com/map/2", "htt
         "http://www.delugerpg.com/map/8",
         "http://www.delugerpg.com/map/9", "http://www.delugerpg.com/map/10", "http://www.delugerpg.com/map/11", 
         "http://www.delugerpg.com/map/12", "http://www.delugerpg.com/map/14", "http://www.delugerpg.com/map/snowcave1",
-        "http://www.delugerpg.com/map/volcano2", "http://www.delugerpg.com/map/powerplant2", "https://www.delugerpg.com/map/pkmnmansion1"
+        "http://www.delugerpg.com/map/volcano2", "http://www.delugerpg.com/map/powerplant2", "https://www.delugerpg.com/map/pkmnmansion1",
+        "https://www.delugerpg.com/map/pkmntower2", "https://www.delugerpg.com/map/pkmntower3"
     ]
